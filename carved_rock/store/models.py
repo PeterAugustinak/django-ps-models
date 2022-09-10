1fbcb4ff42a17c15d1f764788b56811076488296
p-a <peter.augustinak@gmail.com>
2022-09-10 12:27:06 +0200

    Examples of custom methods, functions, Meta class and inheritance, also added forgotten migrations

--- carved_rock/store/models.py
+++ carved_rock/store/models.py
@@ -1,5 +1,13 @@
-from django.db import models
+from decimal import Decimal
 
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+class ProductInStockQuerySet(models.QuerySet):
+    def in_stock(self):
+        return self.filter(stock_count__gt=0)
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -13,10 +21,40 @@ class Product(models.Model):
         help_text="Please add unique value",
         default="a"
     )
+    # slug = models.SlugField()
+    # now Product can be used with .objects and with .stock (where only product
+    # that are in stock will be
+    objects = ProductInStockQuerySet.as_manager()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+    class Meta:
+        db_table = 'product'
+        ordering = ['price', 'name']
+        # constraints = [
+        #     models.CheckConstraint(check=models.Q(price__gte=0),
+        #                            name='price_not_negative_check')
+        #                ]
+
+    @property
+    def vat(self):
+        return Decimal(.2) * self.price
+
+    def get_absolute_url(self):
+        return reverse("store:product-detail", kwargs={'pk': self.id})
 
     def __str__(self):
         return f"product: {self.name}: {self.price} EUR, left: " \
                f"{self.stock_count}"
+
+class OrderedProducts(Product):
+    class Meta:
+        proxy = True
+        ordering = ['name']
 
 
 class ProductImage(models.Model):
@@ -26,6 +64,9 @@ class ProductImage(models.Model):
         'Product', on_delete=models.CASCADE, related_name="images"
     )
 
+    class Meta:
+        db_table = 'product_image'
+
     def __str__(self):
         return f"The image for product {self.product.name}"
 
@@ -33,6 +74,11 @@ class ProductImage(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=30)
     products = models.ManyToManyField('Product', related_name="categories")
+
+    class Meta:
+        db_table = 'category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
 
     def __str__(self):
         return f"Category {self.name}"
---
+++ carved_rock/store/urls.py
@@ -0,0 +1,11 @@
+"""Url mappings for meetings app"""
+
+from django.urls import path
+
+from . import views
+
+app_name = "store"
+
+urlpatterns = [
+    path('category/<str:name>', views.category_view, name="category"),
+]
